20dc562144935fae0a35aaff81f821de23f32ffa
welvins <johnwelvins@gmail.com>
2020-05-25 23:21:00 -0300

    Correcao da condicinante  if (BXN[4] > 0){

--- examples/CANrecive/CANrecive.ino
+++ examples/CANrecive/CANrecive.ino
@@ -30,7 +30,7 @@ void loop() {
   mcp.readDATA(BX0, BX1);
 
   //Se dado tiver sido recebido em TB0 imprime
-  if (BX0[0] > 0){
+  if (BX0[4] > 0){
     for(uint8_t i=0; i < 4; i++){
       Serial.print(BX0[i], HEX);
     }
@@ -43,7 +43,7 @@ void loop() {
     }
   
   //Se dado tiver sido recebido em TB0 imprime
-  if(BX1[0] > 0){
+  if(BX1[4] > 0){
     for(uint8_t i=0; i < 4; i++){
       Serial.print(BX1[i], HEX);
     }
